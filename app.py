51dbc90c2b4561eb5b6be0390e76c361542f298e
daniel-j-feng <fengj.daniel@gmail.com>
2026-04-07 19:26:55 +0000

    display general setup updates

--- app.py
+++ app.py
@@ -4,23 +4,26 @@ import matplotlib.pyplot as plt
 stop_names = []
 stop_counts = []
 
-def add_stop(name, count):
+def addStop(name, count):
     stop_names.append(name)
     stop_counts.append(count)
-    return plot_stops()
+    return plotStops()
 
-def remove_stop(name):
+def removeStop(name):
     if name in stop_names:
         idx = stop_names.index(name)
         stop_names.pop(idx)
         stop_counts.pop(idx)
-    return plot_stops()
+    return plotStops()
 
-def plot_stops():
+def plotStops():
     fig, ax = plt.subplots()
     ax.bar(stop_names, stop_counts, color="steelblue")
     ax.set_xlabel("Stop"); ax.set_ylabel("Crowd Count")
     return fig
+
+def sortStops():
+
 
 with gr.Blocks() as app:
     with gr.Row():
@@ -31,7 +34,7 @@ with gr.Blocks() as app:
     remove_btn = gr.Button("Remove Stop")
     chart = gr.Plot()
 
-    add_btn.click(add_stop, [name_in, count_in], chart)
-    remove_btn.click(remove_stop, remove_in, chart)
+    add_btn.click(addStop, [name_in, count_in], chart)
+    remove_btn.click(removeStop, remove_in, chart)
 
 app.launch()
